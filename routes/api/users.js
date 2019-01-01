394796416b09b6b0df3d2562ce4122b5d7093227
BYUNGMIN CHOI <byung803@gmail.com>
2019-01-01 13:13:10 +0900

    User API Routes & JWT Auth done

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const keys = require('../../config/keys');
 
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //Load User Model
 const User = require('../../models/User');
@@ -21,10 +24,18 @@ router.get('/test', (req, res) => {
 //@desc Register user
 //@access Public 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    //Check Validation 
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
-                return res.status(400).json({ email: 'Email already exists' });
+                errors.email = 'Email already exists';
+                return res.status(400).json(errors);
             } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200', // Size
@@ -56,6 +67,13 @@ router.post('/register', (req, res) => {
 //@desc Login User / Returning Token
 //@access Public 
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    //Check Validation 
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -64,7 +82,8 @@ router.post('/login', (req, res) => {
         .then(user => {
             //Check for user 
             if (!user) {
-                return res.status(404).json({ email: 'User not found' });
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
             //Check Password
             bcrypt.compare(password, user.password).then(isMatch => {
@@ -85,7 +104,8 @@ router.post('/login', (req, res) => {
                         });
 
                 } else {
-                    return res.status(400).json({ password: 'Password incorrect' });
+                    errors.password = 'Password Incorrect';
+                    return res.status(400).json(errors);
                 }
             })
                 .catch(err => console.log('err: ', err));
