390b6b9e8e7a2ba7ef2d3edb5f7535ae9e3b0ad7
BYUNGMIN CHOI <byung803@gmail.com>
2019-02-08 23:44:07 +0900

    Posts & Comments

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -120,10 +120,26 @@ router.post(
     const profileFields = {};
     profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = req.body.company;
-    if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.company) {
+      profileFields.company = req.body.company;
+    } else {
+      profileFields.company = null;
+    }
+    if (req.body.website) {
+      profileFields.website = req.body.website;
+    } else {
+      profileFields.website = null;
+    }
+    if (req.body.location) {
+      profileFields.location = req.body.location;
+    } else {
+      profileFields.location = null;
+    }
+    if (req.body.bio) {
+      profileFields.bio = req.body.bio;
+    } else {
+      profileFields.bio = null;
+    }
     if (req.body.status) profileFields.status = req.body.status;
     if (req.body.githubusername) {
       profileFields.githubusername = req.body.githubusername;
